464a21831e868912bb2e1159f2c3c07a563dbdf0
supradeep <supradeep70@gmail.com>
2025-11-04 15:51:11 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.router.js";
+
+dotenv.config();
+const app=express();
+
+const PORT=process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+app.listen(PORT,()=>console.log("server has started"));
