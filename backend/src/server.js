9a016e37d9df13811ad29eadad116f6b65dda753
supradeep <supradeep70@gmail.com>
2025-11-05 11:32:59 +0530

    deploying fro the first time

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.router.js";
+import path from "path";
 
 dotenv.config();
 const app=express();
 
+const  __dirname=path.resolve();
+
 const PORT=process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
+
+if(process.env.NODE_ENV="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
 app.listen(PORT,()=>console.log("server has started"));
